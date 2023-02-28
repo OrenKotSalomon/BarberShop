690d4b6d0152d338d651e89446e058efe267e202
Harel Natan <harelnatan7@gmail.com>
2023-02-28 10:46:19 +0200

    Before conflicts

--- frontend/src/pages/client-index.tsx
+++ frontend/src/pages/client-index.tsx
@@ -4,19 +4,25 @@ import { useSelector } from 'react-redux'
 import { clientService } from "../services/client-service";
 import { ClientPricing } from "./client-pricing";
 import { ClientState } from "../store/client.reducer";
+import { Client } from "../models/client-model";
+
+interface IClient {
+  client: Client
+}
+const selectClient = (state: ClientState) => state.client
 
 export function ClientIndex() {
   const { clientId } = useParams();
 //   const [client, setClient] = useState(null);
-const client = useSelector<ClientState, ClientState["client"]>((state) => state.client)
+// const client = useSelector<ClientState, IClient>(selectClient)
 
 //   useEffect(() => {
 //   }, []);
 
-if (!client) return <div>Loading...</div>
+// if (!client) return <div>Loading...</div>
   return (
     <section className="client-index">
-      <h1>Client Index</h1>
+      <h1>Welcome to {clientId}</h1>
       <ClientPricing />
     </section>
   );
